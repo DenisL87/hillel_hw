4014a4335bae7b713c2faa9396966716f008f9c5
DenisL87 <denis.litvinenko@yahoo.com>
2020-07-14 18:16:29 +0300

    added new files

---
+++ HW1/src/Task3.java
@@ -0,0 +1,14 @@
+public class Task3 {
+
+	public static void main (String[] args) {
+		int array [] = {18, 48, 54, 22, 69, 987, 4};
+		
+			for (int i = 0; i < array.length; i++) {
+				System.out.println (array[i]);
+			}
+
+			for (int i = 0; i < array.length; i++) {
+				System.out.print (array[i] + " ");
+			}
+	}
+}
