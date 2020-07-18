1f9236da5ec23f902970978d30084277a93570e6
DenisL87 <denis.litvinenko@yahoo.com>
2020-07-18 20:45:31 +0300

    ...

--- HW4/src/Task2.java
+++ HW4/src/Task2.java
@@ -1,13 +1,14 @@
 public class Task2 {
 	
 	public static void main (String[] args) {
-		int[] arr = {556, 999, 21, 45, 79, 31000, 49, 5};
-		int temp = 0;
+
+	int[] arr = {556, 999, 21, 45, 79, 31000, 49, 5};
+	int temp = 0;
         int longest = 0;
         int longestNumDigits = 0;
         int shortest = 0;
                                 		
-			for (int i = 0; i < arr.length; i++) {
+	for (int i = 0; i < arr.length; i++) {
                 int count = 1;
                 temp = arr[i];
 				
@@ -27,8 +28,8 @@ public class Task2 {
 
             }
 			
-		int shortestNumDigits = longestNumDigits;
-            for (int i = 0; i < arr.length; i++) {
+	int shortestNumDigits = longestNumDigits;
+	for (int i = 0; i < arr.length; i++) {
                 int count = 1;
                 temp = arr[i];
                     
@@ -46,7 +47,7 @@ public class Task2 {
                     shortest = arr[i];
                 }
             }
-		System.out.println("The longest number is " + longest + ", consists of " + longestNumDigits + " digit(s).");
-		System.out.println("The shortest number is " + shortest + ", consists of " + shortestNumDigits + " digit(s).");
+	System.out.println("The longest number is " + longest + ", consists of " + longestNumDigits + " digit(s).");
+	System.out.println("The shortest number is " + shortest + ", consists of " + shortestNumDigits + " digit(s).");
     }
 }
