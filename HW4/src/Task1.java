6aa0e2ed2eecaa11cc43325ec2f1ff2a9a86c9ba
DenisL87 <denis.litvinenko@yahoo.com>
2020-07-18 20:49:26 +0300

    ...

--- HW4/src/Task1.java
+++ HW4/src/Task1.java
@@ -1,35 +1,36 @@
 public class Task1 {
 	
 	public static void main (String[] args){
-		int[] arrN = {12, 6, 8, 98, 71, 55, 56, 32, 8, 14};
-		int[] arrK = {8, 52, 98, 47, 71, 8};
-		int[] tempArr = new int[arrK.length];
-		int arrCount = 0;
+
+	int[] arrN = {12, 6, 8, 98, 71, 55, 56, 32, 8, 14};
+	int[] arrK = {8, 52, 98, 47, 71, 8};
+	int[] tempArr = new int[arrK.length];
+	int arrCount = 0;
 		
-		for (int i = 0; i < arrK.length; i++) {
-			for (int y = arrCount; y < arrN.length; y++) {
-				if (arrK[i] != arrN[y]) {
-					arrCount++;
-				}else {
-					tempArr[i] = arrN[arrCount];
-					break;
-				}
-			}
-		}
-			
-		int count = 0;
-		for (int i = 0; i < arrK.length; i++) {
-			if (arrK[i] == tempArr[i]) {
-				count++;
+	for (int i = 0; i < arrK.length; i++) {
+		for (int y = arrCount; y < arrN.length; y++) {
+			if (arrK[i] != arrN[y]) {
+				arrCount++;
 			}else {
+				tempArr[i] = arrN[arrCount];
 				break;
 			}
 		}
+	}
 		
-		if (count == arrK.length) {
-			System.out.print ("Is a subsequence");
+	int count = 0;
+	for (int i = 0; i < arrK.length; i++) {
+		if (arrK[i] == tempArr[i]) {
+			count++;
 		}else {
-			System.out.print ("Isn't a subsequence");
+			break;
 		}
+	}
+	
+	if (count == arrK.length) {
+		System.out.print ("Is a subsequence");
+	}else {
+		System.out.print ("Isn't a subsequence");
+	}
     }
 }
