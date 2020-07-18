539fbd70ff48154b4bd6870d5d1484416597f049
DenisL87 <denis.litvinenko@yahoo.com>
2020-07-18 22:53:10 +0300

    ...

--- HW4/src/Task4.java
+++ HW4/src/Task4.java
@@ -1,11 +1,18 @@
 public class Task4 {
 	
 	public static void main (String[] args) {
-		int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-		int count = 0;
+
+	int[] arr = {8, 6, 1, 8, 0, 6, 7, 2, 9};
+	int count = 0;
 		
-		for (int i = 0; i < 3; i++) {
-			for (int y = 0; y < 3; y++) {
+		for (int i = 0; i < Math.sqrt(arr.length); i++) {
+			if (count == arr.length) {
+					break;
+				}
+			for (int y = 0; y < Math.sqrt(arr.length); y++) {
+				if (count == arr.length) {
+					break;
+				}
 				System.out.print (arr[count] + " ");
 				count++;
 			}
