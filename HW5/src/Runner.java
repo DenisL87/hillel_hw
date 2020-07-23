4f1ccd00d751149169d5434a8fdaaa8524d43538
DenisL87 <denis.litvinenko@yahoo.com>
2020-07-23 23:38:08 +0300

    ...

--- HW5/src/Runner.java
+++ HW5/src/Runner.java
@@ -5,9 +5,9 @@ public class Runner {
         newCar [1] = new Car (2, 2012, 12000, 4465, "Honda", "Civic", "red");
         newCar [2] = new Car (3, 2015, 32000, 2398, "BMW", "x5", "black");
         newCar [3] = new Car (4, 2018, 20000, 1502, "Ford", "Mondeo", "grey");
-        newCar [4] = new Car (4, 2013, 20000, 1982, "Ford", "Mondeo", "grey");
-        newCar [5] = new Car (5, 2019, 19500, 6599, "Peugeot", "308", "silver");
-        newCar [6] = new Car (5, 2019, 19500, 7399, "Peugeot", "307", "yellow");
+        newCar [4] = new Car (5, 2013, 12000, 1982, "Ford", "Mondeo", "pink");
+        newCar [5] = new Car (6, 2019, 19500, 6599, "Peugeot", "308", "silver");
+        newCar [6] = new Car (7, 2015, 18500, 7399, "Peugeot", "307", "yellow");
         int operPeriodToCompare = 5;
         int priceToCompare = 10000;
         int prodYearSpec = 2019;
