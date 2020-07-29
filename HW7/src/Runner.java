ae88e85b683fe2f45c1ddb476ef3ad224b80dfd9
DenisL87 <denis.litvinenko@yahoo.com>
2020-07-29 21:51:21 +0300

    ...

--- HW7/src/Runner.java
+++ HW7/src/Runner.java
@@ -9,10 +9,11 @@ public class Runner {
         l[0] = new Lorry("KAMAZ", "65206", 45000, 100, 20);
         l[1] = new Lorry("MAZ", "4570", 45000, 75, 30);
         l[2] = new Lorry("MAN", "65206", 45000, 100, 40);
-        l[3] = new Lorry("MAZ", "4570", 45000, 75, 8);
+        l[3] = new Lorry("KRAZ", "4570", 45000, 75, 8);
         l[4] = new Lorry("Scania", "65206", 45000, 100, 14);
         l[5] = new Lorry("Renault", "65206", 45000, 100, 44);
-        tf.findSpeed(l, 80, 120);
-        tf.findFuelConsumption(l);
+        //tf.findSpeed(l, 80, 120);
+        //tf.findFuelConsumption(l);
+        //tf.findSpeed(l, 150, 200);
     }
 }
