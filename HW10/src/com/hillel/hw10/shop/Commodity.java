510ab2c0cc991a712864c90e1f90a8bfa12a1d53
DenisL87 <denis.litvinenko@yahoo.com>
2020-08-18 19:02:12 +0300

    ...

--- HW10/src/com/hillel/hw10/shop/Commodity.java
+++
@@ -1,8 +0,0 @@
-package com.hillel.hw10.shop;
-
-public abstract class Commodity {
-    abstract String getName();
-    abstract double getPrice();
-    abstract double getStemLength();
-    abstract int getDaysToLive();
-}
