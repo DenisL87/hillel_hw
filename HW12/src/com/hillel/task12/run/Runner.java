b3b404da2825ab6243ec2a84168f5eb78e6cd8d6
DenisL87 <denis.litvinenko@yahoo.com>
2020-09-07 11:22:47 +0300

    ...

--- HW12/src/com/hillel/task12/run/Runner.java
+++ HW12/src/com/hillel/task12/run/Runner.java
@@ -6,27 +6,53 @@ import com.hillel.task12.util.Util;
 import java.io.*;
 
 public class Runner {
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         Bus[] bus = new Bus[0];
+        Lorry[] lorry = new Lorry[0];
+        LightVehicle[] lv = new LightVehicle[0];
+        FileInputStream fis = new FileInputStream("Fleet.txt");
         Util ut = new Util();
         TaxiFleet tf = new TaxiFleet();
-        int i;
-        char[] c = new char[0];
-        String[] strings = new String[0];
-        String charString = null;
-        FileInputStream fis = new FileInputStream("Fleet.txt");
-        do {
-            i = fis.read();
-            if ((char) i != ','){
-                c = ut.extendArr(c, (char) i);
-                charString = new String(c);
-            }else {
-                strings = ut.createString(strings, charString);
-                c = new char[0];
+        String s = ut.createString(fis);
+        String[] lines = s.split(";");
+        for (int i = 0; i < lines.length; i++) {
+            lines[i] = lines[i].trim();
+        }
+        for (int i = 0; i < lines.length; i++) {
+            String[] prop = lines[i].split(",");
+            switch (prop[0]) {
+                case "bus":
+                    bus = tf.extendArr(bus, new Bus(prop[1].trim(), prop[2].trim(), Integer.parseInt(prop[3].trim()),
+                            Integer.parseInt(prop[4].trim()), Double.parseDouble(prop[5].trim()), Integer.parseInt(prop[6].trim())));
+                    break;
+                case "lorry":
+                    lorry = tf.extendArr(lorry, new Lorry(prop[1].trim(), prop[2].trim(), Integer.parseInt(prop[3].trim()), Integer.parseInt(prop[4].trim()),
+                            Double.parseDouble(prop[5].trim()), Double.parseDouble(prop[6].trim())));
+                    break;
+                case "light vehicle":
+                    lv = tf.extendArr(lv, new LightVehicle(prop[1].trim(), prop[2].trim(), Integer.parseInt(prop[3].trim()),
+                            Integer.parseInt(prop[4].trim()), Double.parseDouble(prop[5].trim())));
+
             }
-        }while (i != -1);
-        //System.out.println(strings[5]);
+        }
         fis.close();
-        //FileOutputStream fos = new FileOutputStream("Fleet.txt");
+        tf.sortByFuelConsumption(bus);
+        FileOutputStream fos = new FileOutputStream("Fleet2.txt");
+        String joinProperties;
+        for (int i = 0; i < bus.length; i++){
+            joinProperties = "bus: " + bus[i].getBrand() + ", " + bus[i].getModel() + ", " + Integer.toString(bus[i].getPrice()) + ", " +
+                    Integer.toString(bus[i].getSpeed()) + ", " + Double.toString(bus[i].getFuelConsumption()) + ", " + Integer.toString(bus[i].getNoOfSeats())  + ";\n";
+            fos.write(joinProperties.getBytes());
+        }
+        for (int i = 0; i < lorry.length; i++){
+            joinProperties = "lorry: " + lorry[i].getBrand() + ", " + lorry[i].getModel() + ", " + Integer.toString(lorry[i].getPrice()) + ", " +
+                    Integer.toString(lorry[i].getSpeed()) + ", " + Double.toString(lorry[i].getFuelConsumption()) + ", " + Double.toString(lorry[i].getCarryingCapacity())  + ";\n";
+            fos.write(joinProperties.getBytes());
+        }
+        for (int i = 0; i < lv.length; i++){
+            joinProperties = "light vehicle: " + lv[i].getBrand() + ", " + lv[i].getModel() + ", " + Integer.toString(lv[i].getPrice()) + ", " +
+                    Integer.toString(lv[i].getSpeed()) + ", " + Double.toString(lv[i].getFuelConsumption()) + ";\n";
+            fos.write(joinProperties.getBytes());
+        }
     }
 }
--- HW12/src/com/hillel/task12/util/Util.java
+++ HW12/src/com/hillel/task12/util/Util.java
@@ -1,5 +1,8 @@
 package com.hillel.task12.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Util {
     public char[] extendArr (char[] arr, char el){
         char[] temp = new char[arr.length + 1];
@@ -9,12 +12,9 @@ public class Util {
         temp[arr.length] = el;
         return temp;
     }
-    public String[] createString (String[] arr, String el){
-        String[] temp = new String[arr.length + 1];
-        for (int i = 0; i < arr.length; i++){
-            temp[i] = arr[i];
-        }
-        temp[arr.length] = el;
-        return temp;
+    public String createString (FileInputStream fis) throws IOException {
+        byte[] fileBytes = new byte[fis.available()];
+        fis.read(fileBytes);
+        return new String (fileBytes);
     }
 }
